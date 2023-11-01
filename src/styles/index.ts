502b33a4ba449e47680039bd691bccf7676e61cc
Florian Kulig <florian.kulig@web.de>
2023-11-01 22:27:58 +0100

    fix export

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
 export { default as theme } from "./theme";
 export { pageContentExit } from "./keyframes";
-export { POPPINS, INTER } from "./fonts";
+export { POPPINS, JAKARTA } from "./fonts";
 export { default as GlobalStyle } from "./GlobalStyle";
