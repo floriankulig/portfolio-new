29805b803da5a65019aa6745e28c34bd7f213339
floriankulig <florian.kulig@web.de>
2025-01-04 23:28:19 +0100

    formatting

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -4,13 +4,13 @@ export const EMAIL = "florian.kulig@web.de" as const;
 
 export const EASING = [0.6, 0.01, -0.05, 0.95] as any;
 
-export const SCROLL_SPRING : Spring = {
+export const SCROLL_SPRING: Spring = {
   type: "spring",
   stiffness: 70,
-    damping: 20,
-    mass: 1.1,
-    restDelta: 0.0001,
-}
+  damping: 20,
+  mass: 1.1,
+  restDelta: 0.0001,
+};
 
 export const BASE_TRANSITION: Transition = {
   type: "tween",
