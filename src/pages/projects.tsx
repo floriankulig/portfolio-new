7317edae30cdf6c318757a7d42b17f4a1c942541
floriankulig <florian.kulig@web.de>
2025-01-05 12:45:30 +0100

    add: create About, Contact, and Projects pages with dynamic header and curtain layout

---
+++ src/pages/projects.tsx
@@ -0,0 +1,22 @@
+import { Curtain } from "components/layout/Curtain";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
+const Header = dynamic(
+  () => import("components/shared").then((mod) => mod.Header),
+  { ssr: false }
+);
+
+const ProjectsPage = () => {
+  return (
+    <>
+      <Curtain />
+      <Header />
+      <div
+        style={{ background: "red", marginTop: "100px", height: "300vh" }}
+      ></div>
+    </>
+  );
+};
+
+export default ProjectsPage;
