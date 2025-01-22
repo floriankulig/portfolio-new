47ee638bd56f769227ea136e1b3675bc2baa911d
floriankulig <florian.kulig@web.de>
2025-01-22 22:22:47 +0100

    fix: change image property in Project interface from optional to required

--- src/ts/content.ts
+++ src/ts/content.ts
@@ -2,7 +2,7 @@ export interface Project {
   id: string;
   title: string;
   description: string;
-  image?: string;
+  image: string;
   technologies: string[];
   technologiesFeatured: number;
   externalLink?: string;
