95751279dcb24ac969cea49e2274e489d8d80e2b
floriankulig <florian.kulig@web.de>
2025-01-22 22:22:54 +0100

    feat: add getProjectByID function to retrieve project by ID from PROJECTS

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,5 @@
 import { MeasurementRect } from "hooks/useMeasure";
+import { PROJECTS } from "ts/content";
 
 export const copyToClipboard = (text: string) => {
   navigator.clipboard.writeText(text);
@@ -12,4 +13,8 @@ export const measureDistance = (
   const dy = rect2.top - rect1.top;
   console.log(`${rect2.left} - ${rect1.left} = ${dx}`);
   return { distanceX: dx, distanceY: dy };
+};
+
+export const getProjectByID = (projectID: string) => {
+  return PROJECTS.find((project) => project.id === projectID);
 };
