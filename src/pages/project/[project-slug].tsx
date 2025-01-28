f7790668280e627e10f646668563e7496d494c87
floriankulig <florian.kulig@web.de>
2025-01-28 18:03:56 +0100

    refactor project detail page to use static generation for improved performance

--- src/pages/project/[project-slug].tsx
+++ src/pages/project/[project-slug].tsx
@@ -2,49 +2,40 @@ import { Curtain } from "components/layout/Curtain";
 import { Description, ProjectDetailHead } from "components/project-detail";
 import { getProjectByID } from "helpers";
 import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import { Project } from "ts/types";
+import { PROJECTS } from "ts/content";
+
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+} from "next";
 
 const Header = dynamic(
   () => import("components/shared").then((mod) => mod.Header),
   { ssr: false }
 );
 
-const ProjectDetailPage = () => {
-  const router = useRouter();
-  const projectID = router.query["project-slug"] as string;
-  const [project, setProject] = useState<Project>(getProjectByID(projectID)!);
+export const getStaticPaths = (async () => {
+  const paths = PROJECTS.map((project) => ({
+    params: { "project-slug": project.id },
+  }));
 
-  useEffect(() => {
-    const fetchedProject = getProjectByID(projectID);
-    if (projectID && fetchedProject) {
-      setProject(fetchedProject);
-    }
-  }, [projectID]);
+  // { fallback: false } means other routes should 404
+  return { paths, fallback: false };
+}) satisfies GetStaticPaths;
 
-  if (!project) {
-    return (
-      <>
-        <Curtain />
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 100,
-            width: "100dvw",
-          }}
-        >
-          <Header />
-        </div>
-        <div style={{ minHeight: "100vh" }}>
-          <h1>Project not found</h1>
-        </div>
-      </>
-    );
-  }
+export const getStaticProps = (async (context) => {
+  const projectID = context.params!["project-slug"] as string;
+  const fetchedProject = getProjectByID(projectID)!;
+  return { props: { project: fetchedProject } };
+}) satisfies GetStaticProps<{
+  project: Project;
+}>;
 
+const ProjectDetailPage = ({
+  project,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Curtain />
