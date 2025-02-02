3cf31288af2173c3a3af29754cec11f2c0e1555f
floriankulig <florian.kulig@web.de>
2025-02-02 18:57:54 +0100

    refactor: update theme colors for improved contrast and consistency

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,9 @@
 //ALL STYLES INHERIT FROM THIS FILE
 const theme = {
-  text1: `#181818`,
-  text2: `#242424`,
+  text1: `#18181c`,
+  text2: `#242428`,
   text3: `#a1a4a8`,
-  bg1: `#f9f9f9`,
+  bg1: `#f9f9fa`,
   bg2: `#ffffff`,
   bg3: `#e3e5e8`,
   easing: [0.6, 0.01, -0.05, 0.95],
