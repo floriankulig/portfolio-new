4b37cd05f053969d0694276eadb19afec030803c
floriankulig <florian.kulig@web.de>
2025-03-18 20:25:50 +0100

    fix: add favicon and web manifest files for improved site branding and mobile support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,25 @@ export default function App({ Component, pageProps, router }: AppProps) {
         <title>Florian Kulig | Portfolio</title>
         <meta name="description" content="Portfolio of Florian Kulig" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="site.webmanifest"></link>
       </Head>
       <style jsx global>{`
         :root {
